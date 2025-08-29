8c4b1a50ae99388c3f658256c9fd455eb82a8b0c
Kevin Casareo <Kevin.Casareo@uts.edu.au>
2025-08-29 17:29:58 +1000

    Added behavior to source files

--- src/parse.c
+++ src/parse.c
@@ -10,8 +10,10 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
 
+
+void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+  
 }
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
@@ -31,7 +33,17 @@ int validate_db_header(int fd, struct dbheader_t **headerOut) {
 }
 
 int create_db_header(int fd, struct dbheader_t **headerOut) {
-	
+  struct dbheader_t *header = calloc(1,sizeof(struct dbheader_t));
+  if (!header) {
+    printf("Malloc failed to create db header\n");
+    return STATUS_ERROR;
+  }
+  header->version = 0x1;
+  header->count = 0;
+  header->magic = HEADER_MAGIC;
+  header->filesize = sizeof(struct dbheader_t);
+
+  return STATUS_SUCCESS;
 }
 
 
